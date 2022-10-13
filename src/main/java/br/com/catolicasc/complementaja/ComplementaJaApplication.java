b1872c4f047195087d6a893dcc2ef1f84e4c1d6c
João Pedro Lazarim <jplazarim@gmail.com>
2022-10-13 11:04:22 -0300

    Criação do projeto spring

---
+++ src/main/java/br/com/catolicasc/complementaja/ComplementaJaApplication.java
@@ -0,0 +1,13 @@
+package br.com.catolicasc.complementaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplementaJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplementaJaApplication.class, args);
+	}
+
+}
